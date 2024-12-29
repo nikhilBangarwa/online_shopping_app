83426fe6a5e98bdd9af963030b54a6da494bc806
nikhilBangarwa <nikhilBangarwa476@gmail.com>
2024-12-29 10:59:22 +0530

    welcome screen and splash  screen

--- lib/screens/auth/welcome_screen.dart
+++ lib/screens/auth/welcome_screen.dart
@@ -1,21 +1,98 @@
 
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import '../../utils/app_constant.dart';
 
-class WelcomeScreen extends StatefulWidget {
-  const WelcomeScreen({super.key});
+class WelcomeScreen extends StatelessWidget {
+  WelcomeScreen({super.key});
 
-  @override
-  State<WelcomeScreen> createState() => _WelcomeScreenState();
-}
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
         backgroundColor: AppConstant.appScendoryColor,
+        title: Text(
+          "Welcome to my app",
+          style: TextStyle(color: AppConstant.appTextColor),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              child: Lottie.asset('assets/splash-icon.json'),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              child: Text(
+                "Happy Shopping",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: Get.height / 12,
+            ),
+            Material(
+              child: Container(
+                width: Get.width / 1.2,
+                height: Get.height / 12,
+                decoration: BoxDecoration(
+                  color: AppConstant.appScendoryColor,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: TextButton.icon(
+                  icon: Image.asset(
+                    'assets/final-google-logo.png',
+                    width: Get.width / 12,
+                    height: Get.height / 12,
+                  ),
+                  label: const Text(
+                    "Sign in with google",
+                    style: TextStyle(color: AppConstant.appTextColor),
+                  ),
+                  onPressed: () {
+
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: Get.height / 50,
+            ),
+            Material(
+              child: Container(
+                width: Get.width / 1.2,
+                height: Get.height / 12,
+                decoration: BoxDecoration(
+                  color: AppConstant.appScendoryColor,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: TextButton.icon(
+                  icon: const Icon(
+                    Icons.email,
+                    color: AppConstant.appTextColor,
+                  ),
+                  label: const Text(
+                    "Sign in with email",
+                    style: TextStyle(color: AppConstant.appTextColor),
+                  ),
+                  onPressed: () {
+
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
