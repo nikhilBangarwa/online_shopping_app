801744b09aa28635a804a227e616d4af6a9bd935
nikhilBangarwa <nikhilBangarwa476@gmail.com>
2024-12-29 10:58:26 +0530

    anyway commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:online_shopping_app/screens/auth/sign_up_screen.dart';
 
-import 'screens/auth/splash_screen.dart';
-import 'screens/user_panel/main_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
 
@@ -21,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home:  const SignUpScreen(),
     );
   }
 }
